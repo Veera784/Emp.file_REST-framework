8e235ef7818e18ea02d3df312efbdf694b295591
unknown <Veera784>
2024-11-09 15:35:26 +0530

    Initial commit

---
+++ pror/mvk/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from.models import employees
+# Register your models hee.
+admin.site.register(employees)
---
+++ pror/mvk/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MvkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mvk'
